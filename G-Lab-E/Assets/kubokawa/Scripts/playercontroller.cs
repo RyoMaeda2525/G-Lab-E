f134d0327a5e6c0284a15b0fec17931e0f2153b7
masakisaki <masaki.kubokawa@gmail.com>
2022-02-15 22:59:54 +0900

    スライムver0.1追加

---
+++ G-Lab-E/Assets/kubokawa/Scripts/playercontroller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0f, 0f, 0.1f * speed);
+            //Instantiate(originObject, this.transform.position, this.transform.rotation);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0f, 0f, -0.1f * speed);
+            //Instantiate(originObject, this.transform.position, this.transform.rotation);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-0.1f * speed, 0f, 0f);
+            //Instantiate(originObject, this.transform.position, this.transform.rotation);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(0.1f * speed, 0f, 0f);
+            //Instantiate(originObject, this.transform.position, this.transform.rotation);
+        }
+    }
+}
