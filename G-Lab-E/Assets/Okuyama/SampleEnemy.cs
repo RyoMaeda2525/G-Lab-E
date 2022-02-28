e7a516071d6ecc1ab51f7e33c273339b1a4b7967
Nana_77 <nana77amery@gmail.com>
2022-02-28 16:01:11 +0900

    エネミーをカメラでみたときに変更

--- G-Lab-E/Assets/Okuyama/SampleEnemy.cs
+++ G-Lab-E/Assets/Okuyama/SampleEnemy.cs
@@ -5,28 +5,44 @@ using UnityEngine.AI;
 
 public class SampleEnemy : MonoBehaviour
 {
+    /// <summary>ナビメッシュ</summary>
     NavMeshAgent m_agent;
     Vector3 _playerPos;
     Transform _playerTransform;
+    /// <summary>プレイヤーとの距離</summary>
+    float distance;
+    /// <summary>カメラに映った時のbool</summary>
+    bool Rendered = false;
+    /// <summary>エネミーが追いかけてくる距離</summary>
+    [SerializeField] float _playerdistance;
+    /// <summary>エネミーのスピード</summary>
     [SerializeField] float speed;
+    /// <summary>追いかけるプレイヤー</summary>
     [SerializeField] GameObject _playerObj;
 
     void Start()
     {
         m_agent = GetComponent<NavMeshAgent>();
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //_playerPos = _playerObj.transform.position;
-        //_playerTransform = _playerObj.transform;
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_playerTransform.position - transform.position), 0.3f);
+        _playerPos = _playerObj.transform.position;
+        distance = Vector3.Distance(this.transform.position, _playerPos);
+        
+        if(Rendered == true/*distance < _playerdistance*/)
+        {
+            //m_agent.SetDestination(transform.position += transform.forward * speed);
+            m_agent.destination = _playerObj.transform.position;
+        }
+    }
 
-        ////targetに向かって進む
-        ////transform.position += transform.forward * speed;
-        //m_agent.SetDestination(transform.position += transform.forward * speed);
-        m_agent.destination = _playerObj.transform.position;
+    /// <summary>メインカメラに映った時に呼ばれる</summary>
+    void OnWillRenderObject()
+    {
+        if (Camera.current.tag == "MainCamera")
+        {
+            Rendered = true;
+        }
     }
 }
