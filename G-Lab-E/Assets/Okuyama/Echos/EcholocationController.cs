98c83799ab67a8816728834186e64ea7579208ba
Nana_77 <nana77amery@gmail.com>
2022-03-05 17:07:47 +0900

    エコーロケーションの作成

---
+++ G-Lab-E/Assets/Okuyama/Echos/EcholocationController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EcholocationController : MonoBehaviour
+{
+    private static readonly int Center = Shader.PropertyToID("_Center");
+    private static readonly int Radius = Shader.PropertyToID("_Radius");
+
+    // ここにインスペクター上でEcholocationマテリアルをセットしておく
+    [SerializeField] private Material material;
+
+    // 半径が大きくなるスピード
+    [SerializeField] [Min(0.0f)] private float speed = 5.0f;
+    [SerializeField] GameObject _player;
+    Vector3 _playerPos;
+
+    // 現在の半径
+    private float radius;
+
+    // 半径の初期値は無限大としておく
+    private void Start()
+    {
+        if(gameObject.name == "Cube")
+        {
+
+        }
+        _playerPos = _player.transform.position;
+        EmitCall(_playerPos);
+    }
+    private void OnEnable()
+    {
+        this.radius = Mathf.Infinity;
+    }
+
+    // 毎フレーム半径のセットおよび拡張を行う
+    private void Update()
+    {
+        this.material.SetFloat(Radius, this.radius);
+        this.radius += this.speed * Time.deltaTime;
+    }
+
+    // 他のスクリプトからEmitCallを実行することで
+    // 中心点を設定し、半径を0にリセットする
+    public void EmitCall(Vector3 position)
+    {
+        this.radius = 0.0f;
+        this.material.SetVector(Center, position);
+    }
+}
---
+++ G-Lab-E/Assets/Okuyama/Echos/EcholocationTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EcholocationController))]
+public class EcholocationTest : MonoBehaviour
+{
+    private EcholocationController controller;
+    private Camera mainCamera;
+
+    //private void Start()
+    //{
+    //    this.mainCamera = Camera.main;
+    //    this.controller = this.GetComponent<EcholocationController>();
+    //}
+
+    //private void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0) && Physics.Raycast(this.mainCamera.ScreenPointToRay(Input.mousePosition), out var hitInfo))
+    //    {
+    //        this.controller.EmitCall(hitInfo.point);
+    //    }
+    //}
+}
