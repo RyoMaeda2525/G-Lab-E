5c2514d50e4d413bd714ff45dc3c4bf087a92bdb
Nana_77 <nana77amery@gmail.com>
2022-04-14 19:24:34 +0900

    送風機と滑る床のギミック

---
+++ G-Lab-E/Assets/Okuyama/IceFloor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceFloor : MonoBehaviour
+{
+    [SerializeField] PhysicMaterial _defaultMaterial, _iceMaterial;
+    float _iceflote = 1f, _defaultflote = 2f;
+    Rigidbody _rb;
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            collider.material = _iceMaterial;
+            _rb = collider.gameObject.GetComponent<Rigidbody>();
+            _rb.drag = _iceflote;
+        }
+    }
+
+    public void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            collider.material = _defaultMaterial;
+            _rb = collider.gameObject.GetComponent<Rigidbody>();
+            _rb.drag = _defaultflote;
+        }
+    }
+}
