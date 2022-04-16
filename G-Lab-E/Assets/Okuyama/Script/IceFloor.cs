5d7502b90a5625816661ecbbdcf7295ad5e6c7b2
Nana_77 <nana77amery@gmail.com>
2022-04-16 15:17:59 +0900

    雪が積もった床ギミックの作成

--- G-Lab-E/Assets/Okuyama/Script/IceFloor.cs
+++ G-Lab-E/Assets/Okuyama/Script/IceFloor.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class IceFloor : MonoBehaviour
 {
-    [SerializeField] PhysicMaterial _defaultMaterial, _iceMaterial;
-    float _iceflote = 1f, _defaultflote = 2f;
+    /// <summary>デフォルトのPhysicMaterial</summary>
+    [SerializeField] PhysicMaterial _defaultMaterial;
+    /// <summary>滑る床のPhysicMaterial</summary>
+    [SerializeField] PhysicMaterial _iceMaterial;
+    /// <summary>デフォルトのdrag値</summary>
+    float _defaultflote = 2f;
+    /// <summary>滑る床のdrag値</summary>
+    [SerializeField] float _iceflote = 1f;
     Rigidbody _rb;
 
     public void OnTriggerEnter(Collider collider)
