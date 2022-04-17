58b7ca6ebf4d3204ec8369c89382e4247f065c79
Nana_77 <nana77amery@gmail.com>
2022-04-17 18:01:14 +0900

    コカトリスの徘徊用

--- G-Lab-E/Assets/Okuyama/Script/SampleEnemy.cs
+++ G-Lab-E/Assets/Okuyama/Script/SampleEnemy.cs
@@ -7,20 +7,49 @@ public class SampleEnemy : MonoBehaviour
 {
     /// <summary>ナビメッシュ</summary>
     NavMeshAgent m_agent;
-    /// <summary>エネミーのスピード</summary>
-    //[SerializeField] float speed = 0.5f;
     /// <summary>追いかけるプレイヤー</summary>
     [SerializeField] GameObject _playerObj;
+    /// <summary>徘徊してほしい場所</summary>
+    [SerializeField] Transform[] _wanderingPoint;
+    [SerializeField] int destPoint = 0;
+
     private RaycastHit hit;
+    /// <summary>徘徊 /デフォ 徘徊する</summary>
+    bool _wanderingBool = true;
 
     void Start()
     {
         m_agent = GetComponent<NavMeshAgent>();
+        m_agent.autoBraking = false;
+
+        GotoNextPoint();
     }
 
     void Update()
     {
-      
+        if (_wanderingBool == true)
+        {
+            if (!m_agent.pathPending && m_agent.remainingDistance < 0.5f)
+            {
+                GotoNextPoint();
+            }
+        }
+    }
+
+    void GotoNextPoint()
+    {
+
+        Debug.Log(_wanderingBool);
+        // 地点がなにも設定されていないときに返します
+        if (_wanderingPoint.Length == 0)
+            return;
+
+        // エージェントが現在設定された目標地点に行くように設定します
+        m_agent.destination = _wanderingPoint[destPoint].position;
+
+        // 配列内の次の位置を目標地点に設定し、
+        // 必要ならば出発地点にもどります
+        destPoint = (destPoint + 1) % _wanderingPoint.Length;
     }
 
     private void OnTriggerStay(Collider other)
@@ -34,19 +63,20 @@ public class SampleEnemy : MonoBehaviour
             if (Physics.Raycast(transform.position, direction, out hit, distance))
             {
                 Debug.Log("Rayが当たった");
-                if (hit.transform.gameObject == _playerObj)
-                {
-                    m_agent.isStopped = false;
-                    m_agent.destination = _playerObj.transform.position;
-                    Debug.Log("当たった");
-                }
-                else
-                {
-                    m_agent.isStopped = true;
-                }
-
+                _wanderingBool = false;
+                m_agent.isStopped = false;
+                m_agent.destination = _playerObj.transform.position;
+            }
+            else
+            {
+                m_agent.isStopped = true;
             }
 
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _wanderingBool = true;;
     }
 }
