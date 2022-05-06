05955a61488c49a850f26ca4a47ce88a9a1b055d
RyoMaeda2525 <maetyuryu@gmail.com>
2022-05-06 17:30:41 +0900

    雪山対処

---
+++ G-Lab-E/Assets/Maeda/Scripts/Respawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField, Tooltip("復活する場所")] GameObject RespawnPosition = default;
+    [SerializeField,Tooltip("暗くなるまでの時間")] int BlackInterval = 10;
+    //[SerializeField] bool respawnBool = false;
+
+    public void ResPawnSet() 
+    {
+        StartCoroutine("ResPawn");
+    }
+
+    IEnumerable ResPawn() 
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        yield return new WaitForSeconds(BlackInterval);
+
+        player.transform.position = RespawnPosition.transform.position;
+
+    }
+}
