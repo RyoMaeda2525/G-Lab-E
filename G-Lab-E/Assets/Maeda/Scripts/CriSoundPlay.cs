3a9bae653d8f2ef582b4c5c99455da37d746b795
RyoMaeda2525 <maetyuryu@gmail.com>
2022-05-17 00:33:37 +0900

    サウンドを再生するスクリプトの追加

---
+++ G-Lab-E/Assets/Maeda/Scripts/CriSoundPlay.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CriSoundPlay : MonoBehaviour
+{
+    CriAtomSource _atomSrc;
+
+    [SerializeField, Tooltip("soundSourceが入っているオブジェクトを選択する")]
+    GameObject soundSrcObject = default;
+
+    [SerializeField , Tooltip("サウンド名登録するためのもの")]
+    string[] soundArray = default; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _atomSrc = soundSrcObject.GetComponent<CriAtomSource>();
+    }
+
+    public void SoundPlay(int a) 
+    {
+        _atomSrc.Play(soundArray[a]);
+
+        CriAtomSource.Status status = _atomSrc.status;
+
+        if (status == CriAtomSource.Status.Prep)
+        {
+            Debug.Log($"{_atomSrc.cueSheet} {_atomSrc.cueName}");
+            Debug.Log("再生準備中");
+        }
+        else 
+        {
+            Debug.Log(_atomSrc.status);
+        }
+    }
+
+    public void PlayAndStopSound()
+    {
+        if (_atomSrc != null)
+        {
+            CriAtomSource.Status status = _atomSrc.status; //CriAtomSource の状態を取得
+            if ((status == CriAtomSource.Status.Stop) || (status == CriAtomSource.Status.PlayEnd))
+            {
+                _atomSrc.Play(); //停止状態なので再生
+            }
+            else
+            {
+                _atomSrc.Stop(); //再生中なので停止
+            }
+        }
+    }
+
+}
